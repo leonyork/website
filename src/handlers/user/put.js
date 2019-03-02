3b63e8e3f769822835fae51deb6591220867ec2b
Leon York <leon@leonyork.com>
2019-03-02 13:48:43 +0000

    Improved logging

--- src/handlers/user/put.js
+++ src/handlers/user/put.js
@@ -5,17 +5,29 @@ const dynamo = require('../../dynamodb.factory')
 const userRepository = new UserRepository(dynamo)
 
 exports.handler = async(event, context) => {
+    let item
     try {
-        const response = await userRepository.put({ id: event.pathParameters.id, content: JSON.parse(event.body) });
+        item = JSON.parse(event.body)
+    } catch (err) {
+        console.log(`Unable to parse event ${JSON.stringify(event)}`);
+        console.log(err);
+        return {
+            statusCode: 400,
+            body: { "message": "Body supplied is not JSON data" }
+        };
+    }
+    try {
+        const response = await userRepository.put({ id: event.pathParameters.id, content: item });
         return {
             statusCode: response ? 200 : 201,
-            body: event.body,
-        }
+            body: item,
+        };
     } catch (err) {
+        console.log(`Error PUTting event ${JSON.stringify(event)}`);
         if (!err.statusCode) { console.log(err); }
         return {
             statusCode: err.statusCode || 500,
-            body: JSON.stringify(err)
-        }
+            body: err
+        };
     }
 };
