6aa32b7e73a94c6a3f6ed9fb1b833e6526f27892
Leon York <leon@leonyork.com>
2019-03-30 12:11:04 +0000

    Speed up deploy

--- build.sh
+++ build.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
 set -e
 STAGE=test$(date +%s%N) 
-pip install awscli --upgrade --user 
-npm install 
+echo "$(date) - *** INSTALLING TOOLS ***"
+pip install awscli --upgrade --user >/dev/null
+npm install >/dev/null
+echo "$(date) - *** RUNNING UNIT TESTS ***"
 npm run unit
 #Serverless seems to not always run the post-deploy hooks to create the script, so just keep retrying in here
 rm scripts/generated/run-tests.sh || true
 while [ ! -f scripts/generated/run-tests.sh ]
 do
-  npm run deploy -- --stage $STAGE 
+  echo "$(date) - *** DEPLOYING TO TEST $STAGE ***"
+  npm run deploy -- --stage $STAGE >/dev/null
 done
+echo "$(date) - *** RUNNING INTEGRATION TESTS ***"
 scripts/generated/run-tests.sh
-npm run remove -- --stage $STAGE 
-npm run deploy -- --stage production
+#Run the remove and deploy in parallel to speed up deployment
+npm run remove -- --stage $STAGE & >/dev/null
+pid_remove=$!
+echo "$(date) - *** DEPLOYING TO PRODUCTION ***"
+npm run deploy -- --stage production &
+pid_deploy=$!
+
+wait $pid_remove
+wait $pid_deploypin
