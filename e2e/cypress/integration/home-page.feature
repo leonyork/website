d3d4b7e4f757d03264783e2570465ceb193039a0
Leon York <leon@leonyork.com>
2019-12-22 00:16:11 +0800

    Adding e2e test

--- e2e/cypress/integration/home-page.feature
+++ e2e/cypress/integration/home-page.feature
@@ -1,6 +1,15 @@
 Feature: Home Page
     Test the home page
 
-    Scenario: Browse to the home page
+    Background:
         Given I visit the home page
+
+    Scenario: 
         Then I see "Leon York" in the title
+
+    Scenario:
+        Then I see "Projects" in the nav bar
+        And I see "Links" in the nav bar
+
+    Scenario: 
+        Then I can browse to the "auth-demo" page
--- e2e/cypress/integration/home-page/home-page.js
+++ e2e/cypress/integration/home-page/home-page.js
@@ -1,9 +1,21 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
  
-Given("I visit the home page", () => {
+Given(`I visit the home page`, () => {
   cy.visit("/")
 })
 
 Then(`I see {string} in the title`, (title) => {
     cy.title().should('include', title)
+})
+
+Then(`I see {string} in the nav bar`, (element) => {
+    cy.get("nav").contains(element)
+})
+
+Then(`I can browse to the {string} page`, (page) => {
+    cy.get(`a[href="${page}"]`).first().click()
+    //This checks that we've navigated to the page
+    cy.location('pathname').should('include', page)
+    //Use this to check that the page was a 200
+    cy.visit(page)
 })
